b257f5de95a59b8a382c7e88cbbe269454287d98
luzai <wxlms@outlook.com>
2016-03-19 19:24:17 +0800

    demo

--- app/src/main/java/com/example/luzai/test/MainActivity.java
+++ app/src/main/java/com/example/luzai/test/MainActivity.java
@@ -1,20 +1,9 @@
 package com.example.luzai.test;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private CheckBox eatBox;
